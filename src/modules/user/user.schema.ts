b16e425098a6fdf654369fa0cfe92e5d809c029e
elforgaani <mohamed.elforgani1999@gmail.com>
2024-07-07 20:45:46 +0200

    Added verify-email, resend-otp schemas!

--- src/modules/user/user.schema.ts
+++ src/modules/user/user.schema.ts
@@ -1,11 +1,11 @@
 import Joi from "joi";
 import { ValidationSchema } from "../../interfaces/ValidationSchema";
 
-export const signUpSchema: ValidationSchema = {
+export const signUpUser: ValidationSchema = {
   body: Joi.object({
     firstName: Joi.string().required(),
-  lastName: Joi.string().required(),
-    userName: Joi.string().required(),
+    lastName: Joi.string().required(),
+    // userName: Joi.string().required(),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8).max(30),
     recoveryEmail: Joi.string().required().email(),
@@ -13,5 +13,18 @@ export const signUpSchema: ValidationSchema = {
     mobileNumber: Joi.string().length(10),
     role: Joi.string().valid(...["user", "company_hr"]),
     status: Joi.string().valid(...["online", "offline"]),
+  }),
+};
+
+export const verifyEmail: ValidationSchema = {
+  body: Joi.object({
+    otp: Joi.string().required().length(6),
+    email: Joi.string().required().email(),
+  }),
+};
+
+export const resendOtp: ValidationSchema = {
+  body: Joi.object({
+    email: Joi.string().required().email(),
   }),
 };
