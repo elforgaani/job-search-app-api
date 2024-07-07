535f3637be540d2ff28fab5615d961a9d97538dc
elforgaani <mohamed.elforgani1999@gmail.com>
2024-07-07 20:44:27 +0200

    Added Otp Model

---
+++ src/database/models/otp.model.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const schema = new Schema({
+  otp: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  expireAt: { type: Date, default: Date.now, expires: 180 },
+});
+
+export default mongoose.models.Otp || model("Otp", schema);
