21883e44d6809ecaa016b6ea5090aeba13212b91
elforgaani <mohamed.elforgani1999@gmail.com>
2024-07-07 20:45:28 +0200

    Added verify-email, resend-otp routes!

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,14 +1,25 @@
 import { Router } from "express";
 import * as UserController from "./user.controller";
 import { validationMiddleware } from "../../middlewares/validation.middleware";
-import { signUpSchema } from "./user.schema";
+import * as UserSchemas from "./user.schema";
 import { errorHanlderMiddleware } from "../../middlewares/error-hanlder.middleware";
 
 const router = Router();
 router.post(
-  "/",
-  validationMiddleware(signUpSchema),
+  "/sign-up",
+  validationMiddleware(UserSchemas.signUpUser),
   errorHanlderMiddleware(UserController.signUp)
+);
+router.post(
+  "/verify-email",
+  validationMiddleware(UserSchemas.verifyEmail),
+  errorHanlderMiddleware(UserController.confirmEmail)
+);
+
+router.post(
+  "/resend-otp",
+  validationMiddleware(UserSchemas.resendOtp),
+  errorHanlderMiddleware(UserController.resendOtp)
 );
 
 export default router;
