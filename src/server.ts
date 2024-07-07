cce977eaffe24d0eb436b5c12d6ce62e1dc8b6ce
elforgaani <mohamed.elforgani1999@gmail.com>
2024-07-07 20:43:46 +0200

    Added User Router to Server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,18 @@
 import express from "express";
 import "dotenv/config";
-import { globalErrorMiddleware } from "./middlewares/global-error.middleware";
+import { globalErrorResponse } from "./middlewares/global-error.middleware";
 import { dbConnection } from "./database/connection";
+import UsersRouter from "./modules/user/user.routes";
+import { sendEmailService } from "./services/send-email.service";
 export const main = () => {
   const app = express();
   const port = parseInt(process.env.PORT || "4000");
-  
+
   dbConnection();
-
+  // sendEmailService();
   app.use(express.json());
-
-  app.use(globalErrorMiddleware);
+  app.use("/api/v1/user", UsersRouter);
+  app.use(globalErrorResponse());
 
   app.listen(port, () => console.log("Server Connected Successfully."));
 };
