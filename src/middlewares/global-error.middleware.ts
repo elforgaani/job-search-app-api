7034daca0fe5ff1faeeb49f16c69ae7658f1848a
elforgaani <mohamed.elforgani1999@gmail.com>
2024-07-07 17:59:40 +0200

    Added GlobalErrorResponse!

--- src/middlewares/global-error.middleware.ts
+++ src/middlewares/global-error.middleware.ts
@@ -1,13 +1,24 @@
 import { NextFunction, Request, Response } from "express";
-import { Error } from "../interfaces/Error";
+import { CustomError } from "../interfaces/CustomError";
+import { date } from "joi";
 
-export const globalErrorMiddleware = () => {
-  return (error: Error, req: Request, res: Response, next: NextFunction) => {
-    if (error) {
+export const globalErrorResponse = () => {
+  return (
+    error: CustomError,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const statusCode = error.statusCode || 500;
+    if (error instanceof CustomError) {
+      res.status(statusCode).json({
+        message: error.errorMessage,
+        data: error.data,
+      });
+    } else {
       res
-        .status(error.statusCode)
-        .json({ success: false, message: error.errorMessage });
+        .status(500)
+        .json({ success: false, message: "Internal Server Error." });
     }
-    res.status(500).json({ success: false, message: "Internal Server Error." });
   };
 };
