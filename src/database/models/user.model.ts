a8ab590f1f4da8d66dc39d79afe9ae02c9aac8ca
elforgaani <mohamed.elforgani1999@gmail.com>
2024-07-07 20:44:02 +0200

    Refactor user model

--- src/database/models/user.model.ts
+++ src/database/models/user.model.ts
@@ -2,47 +2,59 @@ import mongoose from "mongoose";
 
 const { Schema, model } = mongoose;
 
-const schema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
+const schema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    recoveryEmail: {
+      type: String,
+      required: true,
+    },
+    dob: {
+      type: Date,
+      required: true,
+    },
+    mobileNumber: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "company_hr"],
+      default: "user",
+    },
+    status: {
+      type: String,
+      enum: ["online", "offline"],
+      default: "offline",
+    },
+    isConfirmed: {
+      type: Boolean,
+      default: false,
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  recoveryEmail: {
-    type: String,
-    required: true,
-  },
-  dob: {
-    type: Date,
-    required: true,
-  },
-  mobileNumber: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  role: {
-    type: String,
-    enum: ["user", "company_hr"],
-    default: "user",
-  },
-  status: {
-    type: String,
-    enum: ["online", "offline"],
-    default: "offline",
-  },
-});
+  { timestamps: true }
+);
 
 export default mongoose.models.User || model("User", schema);
