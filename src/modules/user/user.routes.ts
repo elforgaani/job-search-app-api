7dc89b8d3922ec12b5f701ca90c212bfd2d427df
elforgaani <mohamed.elforgani1999@gmail.com>
2024-07-07 18:00:03 +0200

    Added User signUp Route!

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,6 +1,14 @@
 import { Router } from "express";
 import * as UserController from "./user.controller";
+import { validationMiddleware } from "../../middlewares/validation.middleware";
+import { signUpSchema } from "./user.schema";
+import { errorHanlderMiddleware } from "../../middlewares/error-hanlder.middleware";
 
 const router = Router();
+router.post(
+  "/",
+  validationMiddleware(signUpSchema),
+  errorHanlderMiddleware(UserController.signUp)
+);
 
 export default router;
