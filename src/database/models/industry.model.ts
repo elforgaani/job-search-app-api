7c9a3252b637b19b661c9b0ee504a7dac0d6dfed
Mohamed Alforjani <m.elforgaani@gmail.com>
2024-07-08 15:46:14 +0200

    Added Industry Model!

---
+++ src/database/models/industry.model.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const schema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    }
+})
+
+export default mongoose.models.Industry || model('Industry', schema);
