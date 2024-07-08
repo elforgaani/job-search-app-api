fe6e90ea67ec680865e6b99e767dfaf953f4fc22
elforgaani <mohamed.elforgani1999@gmail.com>
2024-07-08 20:36:25 +0200

    Added DeleteCompany and UpdateCompany Route

--- src/modules/company/company.routes.ts
+++ src/modules/company/company.routes.ts
@@ -1,17 +1,35 @@
 import { Router } from "express";
 import { authenticationMiddleware } from "../../middlewares/authentication.middleware";
 import { authorizationMiddleware } from "../../middlewares/authorization.middleware";
-import * as CompanyController from '../company/company.controller'
+import * as CompanyController from "../company/company.controller";
 import { errorHanlderMiddleware } from "../../middlewares/error-hanlder.middleware";
 import { validationMiddleware } from "../../middlewares/validation.middleware";
-import * as CompanySchemas from './company.schema'
+import * as CompanySchemas from "./company.schema";
 const router = Router();
 
-router.post('/add-company',
-    authenticationMiddleware,
-    authorizationMiddleware("company_hr"),
-    validationMiddleware(CompanySchemas.addCompany),
-    errorHanlderMiddleware(CompanyController.addCompany)
+router.post(
+  "/add-company",
+  authenticationMiddleware,
+  authorizationMiddleware("company_hr"),
+  validationMiddleware(CompanySchemas.addCompany),
+  errorHanlderMiddleware(CompanyController.addCompany)
 );
+
+router.put(
+  "/update-company",
+  authenticationMiddleware,
+  authorizationMiddleware("company_hr"),
+  validationMiddleware(CompanySchemas.updateCompany),
+  errorHanlderMiddleware(CompanyController.updateCompany)
+);
+
+router.delete(
+  "/delete-company",
+  authenticationMiddleware,
+  authorizationMiddleware("company_hr"),
+  errorHanlderMiddleware(CompanyController.deleteCompany)
+);
+
+router.
 
 export default router;
