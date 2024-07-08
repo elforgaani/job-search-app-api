aa16e967029b70cd4f0212f831cd75d78afc2190
elforgaani <mohamed.elforgani1999@gmail.com>
2024-07-08 20:37:18 +0200

    Added UpdateCompany Schema!

--- src/modules/company/company.schema.ts
+++ src/modules/company/company.schema.ts
@@ -1,20 +1,66 @@
 import Joi from "joi";
 import { ValidationSchema } from "../../interfaces/ValidationSchema";
-import * as ValidationRules from '../../utils/validation-rules.utils'
+import * as ValidationRules from "../../utils/validation-rules.utils";
 export const addCompany: ValidationSchema = {
-    body: Joi.object({
-        companyName: Joi.string().required().min(3).max(30),
-        description: Joi.string().required().min(10).max(200),
-        industry: Joi.string().required().custom(ValidationRules.objectIdRule),
-        address: Joi.object({
-            long: Joi.string().optional(),
-            lat: Joi.string().optional(),
-            country: Joi.string().optional(),
-            city: Joi.string().optional(),
-            zip: Joi.string().optional()
-        }).optional(),
-        numberOfEmployees: Joi.string().required().valid(...['10-20', '21-50', '51-100', '100-200', '200-500', '500-1000', '+100']),
-        companyEmail: Joi.string().email().required(),
-        companyHr: Joi.string().custom(ValidationRules.objectIdRule),
-    })
-}
+  body: Joi.object({
+    companyName: Joi.string().required().min(3).max(30),
+    description: Joi.string().required().min(10).max(200),
+    industry: Joi.string().required().custom(ValidationRules.objectIdRule),
+    address: Joi.object({
+      long: Joi.string().optional(),
+      lat: Joi.string().optional(),
+      country: Joi.string().optional(),
+      city: Joi.string().optional(),
+      zip: Joi.string().optional(),
+    }).optional(),
+    numberOfEmployees: Joi.string()
+      .required()
+      .valid(
+        ...[
+          "10-20",
+          "21-50",
+          "51-100",
+          "100-200",
+          "200-500",
+          "500-1000",
+          "+1000",
+        ]
+      ),
+    companyEmail: Joi.string().email().required(),
+  }),
+};
+export const updateCompany: ValidationSchema = {
+  body: Joi.object({
+    companyName: Joi.string().optional().min(3).max(30),
+    description: Joi.string().optional().min(10).max(200),
+    industry: Joi.string().optional().custom(ValidationRules.objectIdRule),
+    address: Joi.object({
+      long: Joi.string().optional(),
+      lat: Joi.string().optional(),
+      country: Joi.string().optional(),
+      city: Joi.string().optional(),
+      zip: Joi.string().optional(),
+    }).optional(),
+    numberOfEmployees: Joi.string()
+      .optional()
+      .valid(
+        ...[
+          "10-20",
+          "21-50",
+          "51-100",
+          "100-200",
+          "200-500",
+          "500-1000",
+          "+1000",
+        ]
+      ),
+    companyEmail: Joi.string().email().optional(),
+  }).or(
+    "companyName",
+    "description",
+    "industry",
+    " address",
+    " numberOfEmployees",
+    "companyEmail"
+  ),
+};
