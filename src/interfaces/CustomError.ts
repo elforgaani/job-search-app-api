22eb9ab8964bd1b5f842f9f393423d35c51fe574
Mohamed Alforjani <m.elforgaani@gmail.com>
2024-07-08 09:18:13 +0200

    Refactor CustomError Class

--- src/interfaces/CustomError.ts
+++ src/interfaces/CustomError.ts
@@ -4,14 +4,15 @@ export class CustomError extends Error {
   statusCode?: number;
   errorMessage: string;
   data?: any;
+  success?: boolean;
 
-  constructor(errorMessage: string, statusCode?: number, data?: any) {
+  constructor(success: boolean, statusCode: number, errorMessage: string, data?: any,) {
     super(errorMessage);
     this.name = "CustomError";
     this.statusCode = statusCode;
     this.errorMessage = errorMessage;
     this.data = data;
-
+    this.success = success
     // Maintains proper stack trace for where the error was thrown
     if (Error.captureStackTrace) {
       Error.captureStackTrace(this, CustomError);
