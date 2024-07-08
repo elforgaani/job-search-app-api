24ce07bd9e08a53ea3f0a749736768636e1fa29a
Mohamed Alforjani <m.elforgaani@gmail.com>
2024-07-08 15:49:31 +0200

    Refcator Request type Override

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,10 +1,5 @@
-interface User {
-    id: string,
-    email: string,
-    token: string,
-    iat: number,
-    exp: number,
-}
+
+
 declare namespace Express {
     interface Request {
         user: User
