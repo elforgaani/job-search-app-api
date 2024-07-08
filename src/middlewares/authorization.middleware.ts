c0c7f613bb77db598682562235bfe5959829c6bd
elforgaani <mohamed.elforgani1999@gmail.com>
2024-07-08 22:23:26 +0200

    Refactor AuthorizationMiddleware to Accept more than one role!

--- src/middlewares/authorization.middleware.ts
+++ src/middlewares/authorization.middleware.ts
@@ -2,12 +2,18 @@ import { NextFunction, Request, Response } from "express";
 import { AppRoles } from "../types/roles.types";
 import { CustomError } from "../interfaces/CustomError";
 
-export const authorizationMiddleware = (role: AppRoles) => {
-    return (req: Request, res: Response, next: NextFunction) => {
-        const { user } = req;
-        if (!(user.role === role)) {
-            return next(new CustomError(false, 403, "You're not Authorized"));
-        }
-        next();
+export const authorizationMiddleware = (role: AppRoles | AppRoles[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { user } = req;
+    if (Array.isArray(role)) {
+      if (!role.includes(user.role)) {
+        return next(new CustomError(false, 403, "You're not Authorized"));
+      }
+    } else {
+      if (user.role !== role) {
+        return next(new CustomError(false, 403, "You're not Authorized"));
+      }
     }
-}
+    next();
+  };
+};
