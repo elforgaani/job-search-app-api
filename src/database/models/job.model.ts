43312dec89191147c52fb6b85984dc96215d46e9
elforgaani <mohamed.elforgani1999@gmail.com>
2024-07-08 21:54:54 +0200

    Added Job Model!

---
+++ src/database/models/job.model.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const schema = new Schema(
+  {
+    jobTitle: {
+      type: String,
+      required: true,
+    },
+    jobLocation: {
+      type: String,
+      enum: ["onsite", "remotely", "hybrid"],
+      required: true,
+    },
+    seniorityLevel: {
+      type: String,
+      enum: ["junior", "mid-level", "senior", "team-lead", "cto"],
+      required: true,
+    },
+    jobDescription: {
+      type: String,
+      required: true,
+    },
+    technicalSkills: {
+      type: [String],
+      required: true,
+    },
+    softSkills: {
+      type: [String],
+      required: true,
+    },
+    addedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Job || model("Job", schema);
