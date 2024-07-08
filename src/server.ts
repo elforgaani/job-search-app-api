c863537b690e53be614c2317da0a173dc4726d35
Mohamed Alforjani <m.elforgaani@gmail.com>
2024-07-08 16:00:47 +0200

    Added CompanyRouter to server.ts!

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,17 @@ import "dotenv/config";
 import { globalErrorResponse } from "./middlewares/global-error.middleware";
 import { dbConnection } from "./database/connection";
 import UsersRouter from "./modules/user/user.routes";
-import { sendEmailService } from "./services/send-email.service";
+import CompanyRouter from './modules/company/company.routes';
+
 export const main = () => {
   const app = express();
-  const port = parseInt(process.env.PORT || "4000");
 
+  const port = parseInt(process.env.PORT || "4000");
   dbConnection();
   // sendEmailService();
   app.use(express.json());
   app.use("/api/v1/user", UsersRouter);
+  app.use("/api/v1/company", CompanyRouter);
   app.use(globalErrorResponse());
 
   app.listen(port, () => console.log("Server Connected Successfully."));
