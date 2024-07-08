b2d0cf8d2d0d5fd73e5a15ef5ebfef8e396a500c
elforgaani <mohamed.elforgani1999@gmail.com>
2024-07-08 22:04:30 +0200

    Added ApplicationModel!

---
+++ src/database/models/application.model.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const schema = new Schema(
+  {
+    jobId: {
+      type: Schema.Types.ObjectId,
+      ref: "Job",
+      required: true,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    userTechSkills: {
+      type: [String],
+      required: true,
+    },
+    userSoftSkills: {
+      type: [String],
+      required: true,
+    },
+    userResumse: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Application || model("Application", schema);
