35c4225f5746f09307ab3542427b5754e0b94271
Mohamed Alforjani <m.elforgaani@gmail.com>
2024-07-10 08:35:41 +0200

    Refactor Otp Model!

--- src/database/models/otp.model.ts
+++ src/database/models/otp.model.ts
@@ -12,11 +12,6 @@ const schema = new Schema({
     required: true,
     unique: true,
   },
-  mobileNumber: {
-    type: String,
-    unique: true,
-  },
-
   expireAt: { type: Date, default: Date.now, expires: 180 },
 });
 
