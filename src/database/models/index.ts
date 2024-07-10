97d87970dbd2c5045d031bf629fbf8ad4fecf449
elforgaani <mohamed.elforgani1999@gmail.com>
2024-07-10 20:22:11 +0200

    Added index.ts to models dir!

---
+++ src/database/models/index.ts
@@ -0,0 +1,6 @@
+export * from "./application.model";
+export * from "./company.model";
+export * from "./industry.model";
+export * from "./job.model";
+export * from "./otp.model";
+export * from "./user.model";
