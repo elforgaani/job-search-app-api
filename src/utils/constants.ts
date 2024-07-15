814b250dc1f86e8b3aa4a0f00ad008483f330997
elforgaani <mohamed.elforgani1999@gmail.com>
2024-07-15 21:54:15 +0200

    Added all Constant Enums in constants.ts!

---
+++ src/utils/constants.ts
@@ -0,0 +1,31 @@
+// Company Related Enums
+
+export const numberOfEmployeesEnum = [
+  "10-20",
+  "21-50",
+  "51-100",
+  "100-200",
+  "200-500",
+  "500-1000",
+  "+1000",
+];
+
+// User Related Enums
+
+export const roleEnum = ["user", "company_hr"];
+
+export const statusEnum = ["online", "offline"];
+
+// Job Related Enums
+
+export const jobLocationEnum = ["onsite", "remotely", "hybrid"];
+
+export const workingTimeEnum = ["part-time", "full-time"];
+
+export const seniorityLevelEnum = [
+  "junior",
+  "mid-level",
+  "senior",
+  "team-lead",
+  "cto",
+];
